e833ad52ae212315ad7a5c630408bf6dffbbe697
EMCain <>
2018-01-04 14:34:48 -0800

    created FactBook class, added all the facts

--- app/src/main/java/io/github/emcain/factsphere/FactsActivity.java
+++ app/src/main/java/io/github/emcain/factsphere/FactsActivity.java
@@ -11,7 +11,8 @@ import org.w3c.dom.Text;
 import java.util.Random;
 
 public class FactsActivity extends AppCompatActivity {
-    // declare our variables
+    private FactBook factBook = new FactBook();
+    // view declarations
     private TextView factTextView;
     private Button showFactButton;
 
@@ -28,23 +29,7 @@ public class FactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                        "The billionth digit of Pi is 9.",
-                        "Humans can survive underwater. But not for very long.",
-                        "A nanosecond lasts one billionth of a second.",
-                        "Honey does not spoil.",
-                        "The atomic weight of Germanium is seven two point six four.",
-                        "An ostrich's eye is bigger than its brain.",
-                        "Rats cannot throw up.",
-                        "Iguanas can stay underwater for twenty-eight point seven minutes.",
-                        "The moon orbits the Earth every 27.32 days.",
-                        "A gallon of water weighs 8.34 pounds."
-                };
-
-                // Randomly select our fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(10);
-                String fact = facts[randomNumber];
+                String fact = factBook.getFact();
 
                 // change the text to display it
                 factTextView.setText(fact);
